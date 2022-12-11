d9983e5f4dd43f70892be671cfd51b9a1269b90d
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-11 04:07:19 -0800

    pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main function
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char z;
+
+	for (z = 'a' ; z <= 'z' ; z++)
+	{
+		if (z == 'q' && z == 'e')
+			continue;
+		putchar(z);
+	}
+	putchar('\n');
+	return (0);
+}
