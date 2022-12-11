d049cfda2b30e591f5e5d97d79b06695360f829d
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-11 05:05:30 -0800

    hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -13,22 +13,22 @@ int main(void)
 	int x;
 	char z;
 
-	for (x = 0 ; x < 16 ; x++)
-	{
-		if (x == 10)
-			putchar('a');
-		else if (x == 11)
-			putchar('b');
-		else if (x == 12)
-			putchar('c');
-		else if (x == 13)
-			putchar('d');
-		else if (x == 14)
-			putchar('e');
-		else if (x == 15)
-			putchar('f');
-		putchar(x + '0');
-	}
+		if (x > 9 && x < 16)
+		{
+			for (x = 10 ; x < 16 ; x++)
+			{
+				char a = 'a'
+
+				putchar(a);
+				a++;
+			}
+		}
+		if (x >= 0 && x <= 9)
+		{
+			for (x = 0 ; x <= 9 ; x++)
+				putchar(x + '0');
+		}
+
 	putchar('\n');
 	return (0);
 }
