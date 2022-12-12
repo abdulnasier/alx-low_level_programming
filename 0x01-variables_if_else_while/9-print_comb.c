7f7eeb8fab024309118bf9ab62d5752684e76176
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-12 05:56:05 -0800

    combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,13 +14,14 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
+		putchar(i + '0');
+
 		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
 		}
-		else if (i == 9)
-			putchar(i + '0');
 	}
-	return (0); }
+	putchar('\n');
+	return (0);
+}
