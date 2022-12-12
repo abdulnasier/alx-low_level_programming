1e1b26bda5a0212a0ad72f981ff2c1486c113387
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-12 05:26:59 -0800

    combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main- main function
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		putchar(",");
+		putchar(" ");
+	}
+	putchar("\n");
+	return (0);
+}
