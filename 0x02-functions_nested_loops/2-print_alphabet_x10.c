a778bbaaea8cca764a0eef437b966f0197bf4f8b
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-15 14:52:24 -0800

    10X alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function to be created
+ *
+ * Return: Always 0
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int a;
+
+	for (a = 1 ; a <= 10 ; a++)
+	{
+		for (i = 'a' ; i <= 'z' ; i++)
+			_putchar(i);
+	}
+	_putchar('\n');
+
+}
