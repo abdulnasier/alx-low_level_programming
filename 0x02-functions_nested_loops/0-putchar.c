2e5e0ba50aefeb46fcf4f2122e269585b1f0bb8a
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-15 13:53:18 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "header.h"
+
+/**
+ * _putchar - function to be created
+ *
+ * @c: function parameter
+ *
+ * Return: always 0;
+ *
+ */
+
+int _putchar(char c)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
