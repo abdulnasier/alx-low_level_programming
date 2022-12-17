668917a70453490cb65cb6e545f9c063b038ddd7
Abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-17 08:09:26 -0800

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,8 +11,14 @@
  *
  */
 
-int print_last_digit(int a)
+int print_last_digit(int n)
 {
-	printf("%d", a % 100);
-	return (0);
+	int last = n % 10;
+
+	if (n < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
