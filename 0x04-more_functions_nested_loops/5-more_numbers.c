5131a75476e94f6f51a771a768465e72f4126787
abdulnasier <abdulnasirmuhammad5@gmail.com>
2022-12-29 15:31:37 -0800

    10 times numbers print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 	for (i = 0 ; i < 11 ; i++)
 	{
 		for (j = 0 ; j < 15 ; j++)
-			_putchar(j + '0');
+		{
+			if (j > 9)
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
