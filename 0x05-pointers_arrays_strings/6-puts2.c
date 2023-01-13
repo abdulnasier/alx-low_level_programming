2f53f731c8e5e0522ad53999883ef8118a8207be
abdulnasier <abdulnasirmuhammad5@gmail.com>
2023-01-13 07:50:05 -0800

    Half the lies

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - function to print other characters
+ * @str: function parameter
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
